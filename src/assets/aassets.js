93ae78ed31b014a659020421232ef096f4001cbb
Boba? <bondanbanuaji@gmail.com>
2025-02-01 12:59:45 +0700

    create frontend ui of gemini clone

--- src/assets/aassets.js
+++ src/assets/aassets.js
@@ -1,12 +1,12 @@
 import history_icon from './history_icon.png';
-import mennu_icon from './menu_icon.png';
+import menu_icon from './menu_icon.png';
 import plus_icon from './plus_icon.png';
 import question_icon from './question_icon.png';
 import setting_icon from './setting_icon.png';
 import bulb_icon from './bulb_icon.png';
 import compass_icon from './compass_icon.png';
 import gallery_icon from './gallery_icon.png';
-import mic_icon from './set_icon.png';
+import mic_icon from './mic_icon.png';
 import user_icon from './user_icon.jpg';
 import youtube_icon from './youtube_icon.png';
 import message_icon from './message_icon.png';
@@ -16,7 +16,7 @@ import gemini_icon from './gemini_icon.png';
 
 export const assets = {
     history_icon,
-    mennu_icon,
+    menu_icon,
     plus_icon,
     question_icon,
     setting_icon,
@@ -29,5 +29,5 @@ export const assets = {
     message_icon,
     code_icon,
     send_icon,
-    gemini_icon,
+    gemini_icon
 };
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import './Sidebar.css'
+import { assets } from '../../assets/aassets'
+
+const Sidebar = () => {
+    
+    const [extended, setExtended] = useState(false)
+
+    return (
+        <div className='sidebar'>
+            <div className="top">
+                <img onClick={()=>setExtended(prev=>!prev)} className="menu w-5 h-5" src={assets.menu_icon} alt="menu" />
+                <div className="new-chat">
+                    <img className="w-5 h-5" src={assets.plus_icon} alt="plus" />
+                    {extended?<p>New Chat</p>:null}
+                </div>
+                {extended
+                ?<div className="recent">
+                    <p className="recent-title">Recent</p>
+
+                    <div className="recent-entry">
+                        <img className="w-5 h-5" src={assets.message_icon} alt="message" />
+                        <p className="">What is React ...</p>
+                    </div>
+                </div>
+                :null
+            }
+                
+            </div>
+            <div className="bottom">
+                <div className="bottom-item recent-entry">
+                    <img className="w-5 h-5" src={assets.question_icon} alt="" />
+                    {extended?<p>Help</p>:null}
+                </div>
+                <div className="bottom-item recent-entry">
+                    <img className="w-5 h-5" src={assets.history_icon} alt="" />
+                    {extended?<p>Activity</p>:null}
+                </div>
+                <div className="bottom-item recent-entry">
+                    <img className="w-5 h-5" src={assets.setting_icon} alt="" />
+                    {extended?<p>Setings</p>:null}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
